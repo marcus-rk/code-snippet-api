5564762254e4bfeae8d88985fc7b4cd42689c619
marcus-rk <marcus.rappenborg@gmail.com>
2023-11-27 17:43:10 +0100

    hide sections when changing user in main.js

--- public/js-src/main.js
+++ public/js-src/main.js
@@ -1,5 +1,8 @@
 const selectElement = document.querySelector('#logged-in');
 
+// hide sections when changing user
+selectElement.addEventListener('change', hideSections);
+
 // TODO: make this dynamic in options
 function getCurrentUserID() {
     return selectElement.value;
