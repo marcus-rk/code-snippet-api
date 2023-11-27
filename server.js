f34187b84bfe65a93c2f68a73529678ef073e06f
marcus-rk <marcus.rappenborg@gmail.com>
2023-11-27 10:46:27 +0100

    Now connected with MySQL database.

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 // npm install express --save
 const express = require("express");
+
 // npm install mysql2 --save
 const mysql = require("mysql2");
+
 // npm install cors --save
 const cors = require("cors");
+
+// npm install fs --save
+const fs = require('fs');
+
+// password for MySQL from password.js that is on .gitignore
+const password = require('./password');
 
 const app = express();
 const port = 3000;
@@ -14,24 +22,14 @@ app.use(cors()); // avoid network security restrictions
 const connection = mysql.createConnection({
     host:"localhost",
     user:"root",
-    password: passwordFromFile,
-    database:"pokemon"
+    password: password,
+    database:"code_snippet"
 });
 
 app.get('/',(req, res)=>{
-    connection.query('SELECT `name` FROM pokemon',(error,results)=>{
+    connection.query('SELECT * FROM `user`',(error,results)=>{
         res.send(results);
     });
-});
-
-app.get('/pokemon/:name/', (req,res)=>{
-    const pokemonUserRequest = req.params.name;
-    connection.query('SELECT * FROM pokemon WHERE ´name´ = ?',
-        [pokemonUserRequest],
-        (error, results)=>{
-            res.send(results);
-        });
-
 });
 
 app.listen(port, () =>{
